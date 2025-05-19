368d11fba6fd083c9325edbe2335d116ef9c1afe
arfy slowy <slowy.arfy@proton.me>
2025-05-19 20:30:39 +0700

    chore: improving shell scripts (#3)
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- build.sh
+++ build.sh
@@ -4,4 +4,14 @@ make_libs () {
   uv run setup.py build_ext --inplace;
 }
 
-make_libs
+clean_project() {
+  find . -type f $ -name "*.so" -o -name "*.c" -o -name "*.pyc" $ -exec rm -f {} +
+find . -type d -name "__pycache__" -exec rm -rf {} +
+}
+
+if [ "$1" == "make" ]; then
+  make_libs
+elif [ "$1" == "clean" ]; then
+  clean_project
+fi
+
